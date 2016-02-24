07d71cb60685a898e82373d0cc29797d4efc3bfa
Pratiksha <pratiksha.jagadish@capgemini.com>
2016-02-24 15:17:56 +0530

    Update numbers.js

--- app/numbers.js
+++ app/numbers.js
@@ -6,11 +6,17 @@ exports.numbersAnswers = {
   },
 
   base10: function(str) {
-
+	  return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
+	  var arr = [];
 
+	    for (var i = 7; i > -1; i--) {
+	      arr.push( num & (1 << i) ? 1 : 0 );
+	    }
+
+	    return arr.join('');
   },
 
   multiply: function(a, b) {
