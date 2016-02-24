a1da59f719cb43894f568afee6a0a36385e46b0a
Pratiksha <pratiksha.jagadish@capgemini.com>
2016-02-24 15:18:41 +0530

    Update recursion.js

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,32 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
+	  var listOfFiles = [];
+	    var dirs = [];
 
+	    processDir(data);
+
+	    function processDir(dir) {
+	      var i, len, file;
+	      var files = dir.files;
+
+	      dirs.push( dir.dir );
+
+	      for (i = 0, len = files.length; i < len; i++) {
+	        file = files[i];
+	        if (typeof file === 'string') {
+	          if (!dirName || dirs.indexOf(dirName) > -1) {
+	            listOfFiles.push(files[i]);
+	          }
+	        } else {
+	          processDir(files[i]);
+	        }
+	      }
+
+	      dirs.pop();
+	    }
+
+	    return listOfFiles;
   },
 
   permute: function(arr) {
@@ -10,7 +35,14 @@ exports.recursionAnswers = {
   },
 
   fibonacci: function(n) {
-
+	  calcFiboNth(n);
+	  function calcFiboNth(num) {
+	        if (num > 2) {
+	            return calcFiboNth(num - 2) + calcFiboNth(num - 1);
+	        } else {             
+	            return 1;
+	        }
+	    }
   },
 
   validParentheses: function(n) {
