e1a40f94f2294785cbc6173ae8aa6d31c7b9accd
Pratiksha <pratiksha.jagadish@capgemini.com>
2016-02-24 15:16:17 +0530

    Update count.js

--- app/count.js
+++ app/count.js
@@ -2,6 +2,15 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+	  var timeout;
+	    function startCount () {
+	      console.log(start++);
 
+	      if (start <= end) {
+	        timeout = setTimeout(startCount, 100);
+	      }
+	    }
+
+	    startCount();
   }
 };
