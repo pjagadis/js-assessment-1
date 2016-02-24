aca959a09d3e4b93a6bbff4c82d1e474376007f2
Pratiksha <pratiksha.jagadish@capgemini.com>
2016-02-24 15:15:56 +0530

    Update bestPractices.js

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -7,29 +7,35 @@ exports = (typeof window === 'undefined') ? global : window;
  */
 
 exports.bestPracticesAnswers = {
-  globals : function() {
-    myObject = {
-      name : 'Jory'
-    };
+	globals : function() {
+		var myObject = {
+			name : 'Jory'
+		};
 
-    return myObject;
-  },
+		return myObject;
+	},
 
-  functions : function(flag) {
-    if (flag) {
-      function getValue() { return 'a'; }
-    } else {
-      function getValue() { return 'b'; }
-    }
+	functions : function(flag) {
+		var getValue;
 
-    return getValue();
-  },
+		if (flag) {
+			getValue = function() {
+				return "a";
+			}
+		} else {
+			getValue = function() {
+				return "b";
+			}
+		}
 
-  parseInt : function(num) {
-    return parseInt(num);
-  },
+		return getValue();
+	},
 
-  identity : function(val1, val2) {
+	parseInt : function(num) {
+		return parseInt(num, 10);
+	},
 
-  }
+	identity : function(val1, val2) {
+		return val1 === val2;
+	}
 };
